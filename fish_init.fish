480a4af1fa45882d64df830cc790bf4f1c2a0b34
Miha Nedok <mike@inovislabs.com>
2020-03-21 16:42:41 +0100

    Add scripts and custom vars.

--- fish_init.fish
+++ fish_init.fish
@@ -1,1 +1,8 @@
 #!/usr/bin/env fish
+
+
+### Install Oh my Fish
+curl -L https://get.oh-my.fish | fish
+omf install nelsonjchen
+cp mikelxd.fish ~/.config/fish/conf.d/
+cp mikecommon.fish ~/.config/fish/conf.d/
--- ubuntu_install_fish.sh
+++ ubuntu_install_fish.sh
@@ -3,4 +3,4 @@
 ### Fish Install for Ubuntu systems
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
-sudo apt-get install fish
+sudo apt-get install fish -y
